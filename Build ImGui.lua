2148eb6cfe1d95fd6fe637ceb4f773d47de4cf6f
Lanz Ebersöhn <lebersohn@incomargroup.com>
2026-06-25 08:28:17 +0200

    Improven .lua file

--- Build ImGui.lua
+++ Build ImGui.lua
@@ -1,64 +1,45 @@
 project "ImGui"
-   kind "StaticLib"
-   language "C++"
-   cppdialect "C++20"
-   targetdir "Binaries/%{cfg.buildcfg}"
-   staticruntime "off"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
 
+    SetupCommonConfig()
 
-   -- Add the source files to the project
-   files {
-    "imgui_demo.cpp",
-    "imgui_draw.cpp",
-    "imgui_tables.cpp",
-    "imgui_widgets.cpp",
-    "imgui.cpp",
-    "imconfig.h",
-    "imgui_internal.h",
-    "imgui.h",
-    "imstb_rectpack.h",
-    "imstb_textedit.h",
-    "imstv_truetype.h",
-    "backends/imgui_impl_opengl3.cpp",
-    "backends/imgui_impl_opengl3.h",
-    "backends/imgui_impl_glfw.cpp",
-    "backends/imgui_impl_glfw.h",
+    targetdir ("../Binaries/" .. OutputDir .. "/%{prj.name}")
+    objdir ("../Binaries/Intermediates/" .. OutputDir .. "/%{prj.name}")
+
+    files {
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_widgets.cpp",
+        "imgui_demo.cpp",
+
+        "imgui.h",
+        "imgui_internal.h",
+        "imconfig.h",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_glfw.h",
+        "backends/imgui_impl_opengl3.cpp",
+        "backends/imgui_impl_opengl3.h"
     }
-    -- Add the include directories
-   includedirs {
-    "extern/glfw/include",
-    "", --for the root folder
-   }
 
-    -- Add the library directories
-   libdirs {}
-    
-    -- Add the dependant project
-   links {"GLFW"}
+    includedirs {
+        ".",                          -- FIXED (was "")
+        "backends",
+        "../glfw/include"
+    }
 
-   targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
-   objdir ("bin/Intermediates/" .. OutputDir .. "/%{prj.name}")
-
-   filter "system:windows"
-       systemversion "latest"
-       defines { }
-
-   filter "configurations:Debug"
-       defines { "DEBUG" }
-       runtime "Debug"
-       symbols "On"
-
-   filter "configurations:Release"
-       defines { "RELEASE" }
-       runtime "Release"
-       optimize "On"
-       symbols "On"
-
-   filter "configurations:Dist"
-       defines { "DIST" }
-       runtime "Release"
-       optimize "On"
-       symbols "Off"
+    links {
+        "glfw"
+    }
 
     filter "action:vs*"
-       buildoptions { "/MP" }  -- Enable multi-process compilation
+        buildoptions { "/MP" }
+
+    filter {}
